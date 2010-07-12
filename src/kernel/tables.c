6019556c8b52785de9649d84d29c1926bcad6f08
Pablo Herrero <pablodherrero@gmail.com>
2010-07-12 13:16:51 -0300

    Corregir varios problemas en kernel/tables.c

--- src/kernel/tables.c
+++ src/kernel/tables.c
@@ -7,16 +7,16 @@ uint64_t gdt[] __attribute__((__aligned__(16))) = {
     
     /*Null Segment*/ GDT_NULL,
     
-    /*kernel_cs*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFFFFF) |
+    /*kernel_cs*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFF) |
     GDT_DESC_DPL(0x0) | GDT_DESC_TYPE(GDT_F_CODE_R) | COMMON_FLAGS,
     
-    /*kernel_ds*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFFFFF) |
+    /*kernel_ds*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFF) |
     GDT_DESC_DPL(0x0) | GDT_DESC_TYPE(GDT_F_DATA_W) | COMMON_FLAGS,
     
-    /*user_cs*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFFFFF) |
+    /*user_cs*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFF) |
     GDT_DESC_DPL(0x3) | GDT_DESC_TYPE(GDT_F_CODE_R) | COMMON_FLAGS,
     
-    /*user_ds*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFFFFF) |
+    /*user_ds*/ GDT_DESC_BASE(0x0) | GDT_DESC_LIMIT(0xFFFFF) |
     GDT_DESC_DPL(0x3) | GDT_DESC_TYPE(GDT_F_DATA_W) | COMMON_FLAGS };
 
 
