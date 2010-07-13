20e1e933f0fbd4790956eefd0868c3ceb93113d8
Pablo Antonio <pabloa@gmail.com>
2010-07-13 13:52:13 -0300

    Agregar directorio y tabla de paginas iniciales

--- src/kernel/tables.c
+++ src/kernel/tables.c
@@ -1,5 +1,23 @@
 #include "mmu.h"
 
+uint64_t pt_temp_im[128] __attribute__((__aligned__(4096))) = {
+    [PTI(KERNEL_PHYS_ADDR)] = PTE_PAGE_BASE(KERNEL_PHYS_ADDR) | PTE_G(0x1) |
+        PTE_PAT(0x0) | PTE_D(0x0) | PTE_A(0x0) | PTE_PCD(0x0) | PTE_PWT(0x1) |
+        PTE_US(0x0) | PTE_RW(0x1) | PTE_P(0x1)
+};
+
+
+uint64_t pd[128] __attribute__((__aligned__(4096))) = {
+    [PDI(KERNEL_PHYS_ADDR)] = PDE_PT_BASE(&pt_temp_im) | PDE_PS(0x0) |
+        PDE_A(0x0) | PDE_PCD(0x0) | PDE_PWT(0x1) | PDE_US(0x0) | PDE_RW(0x1) |
+        PDE_P(0x1),
+    [PDI(KERNEL_VIRT_ADDR)] = PDE_PT_BASE(&pt_temp_im) | PDE_PS(0x0) |
+        PDE_A(0x0) | PDE_PCD(0x0) | PDE_PWT(0x1) | PDE_US(0x0) | PDE_RW(0x1) |
+        PDE_P(0x1),
+};
+
+
+
 #define COMMON_FLAGS (GDT_DESC_G(0x1) | GDT_DESC_DB(0x1) | GDT_DESC_L(0x0) | \
     GDT_DESC_P(0x1) | GDT_DESC_S(0x1))
 
