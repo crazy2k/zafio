aaad4bf1e9956dca3872c72f0392566e7e6b1ad4
Pablo Antonio <pabloa@gmail.com>
2010-07-19 19:18:40 -0300

    Quitar segmento loco en kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,10 +1,6 @@
 #include "../inc/mmu.h"
 
-unsigned long long int segment = GDT_DESC_BASE(0) | GDT_DESC_LIMIT(0xFFFFF) /*| GDT_TYPE(STA_W) | 
-       GDT_PRIVILEGE_LEVEL(0) | GDT_APPLICATION_SEGMENT | GDT_32_BITS_SEGMENT |
-       GDT_GRANULARITY_LIMIT_4KB | GDT_PRESENT_SEGMENT*/;
-
-void cmain(unsigned int magic_number, void* mbinfo) {
+void cmain() {
 
     unsigned char *video_mem = (unsigned char *) 0xB8000;
     int i;
