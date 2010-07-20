e9078d9e3dc132f9d8b1759fca35351bdc9841ac
Pablo Antonio <pabloa@gmail.com>
2010-07-20 03:14:18 -0300

    Cambiar dir por addr en gdtr

--- src/kernel/tables.c
+++ src/kernel/tables.c
@@ -52,7 +52,7 @@ uint64_t gdt[] __attribute__((aligned(0x08))) = {
 
 struct { 
     uint16_t size __attribute__((packed));
-    uint32_t dir  __attribute__((packed));
+    uint32_t addr __attribute__((packed));
 } gdtr __attribute__((aligned(0x08))) = { sizeof(gdt) - 1, KPHADDR(gdt) }; 
 
 
