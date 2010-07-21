cccc640437b119567a81748ae0b60f6dd5b42271
Pablo Antonio <pabloa@gmail.com>
2010-07-21 03:47:04 -0300

    Arreglar PAGE_TO_PHADDR y escribir comentarios de macros en vvmu.h

--- src/inc/vmmu.h
+++ src/inc/vmmu.h
@@ -7,8 +7,15 @@
 #define RELOCATE_PTR_TO(pointer, virtual_dir) \
 	((void*) ( ((uint32_t)pointer & 0xFFF) | ((uint32_t)virtual_dir & 0xFFFFF000) ))
 
-#define PAGE_TO_PHADDR(obj) ((void *) ((obj) - pages))
-#define PHADDR_TO_PAGE(addr) ((page *) (pages + ((uint32_t) (addr))/PAGE_SIZE ))
+/* Recibe un puntero a ``page``, y devuelve la direccion fisica de la pagina
+ * representada por dicha estructura
+ */
+#define PAGE_TO_PHADDR(obj) ((void *) (((obj) - pages)*PAGE_SIZE))
+
+/* Recibe la direccion fisica de una pagina y devuelve un puntero a la
+ * estructura ``page`` que la representa
+ */
+#define PHADDR_TO_PAGE(addr) ((page *) (pages + ((uint32_t) addr)/PAGE_SIZE))
 
 extern uint32_t kernel_pd[1024];
 extern uint32_t kernel_pt[1024];
