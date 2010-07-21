c94205e64bbd7742bbf1077159ded7463ff09a55
Pablo Antonio <pabloa@gmail.com>
2010-07-21 03:46:30 -0300

    Retocar vmmu.c (mayormente nombres y comentarios)

--- src/kernel/vmmu.c
+++ src/kernel/vmmu.c
@@ -35,82 +35,88 @@ void page_dir_unmap(uint32_t page_dir[], void* virtual) {
 }*/
 
 
-// Obtiene el entry correspondiente a la direccion virtual dentro del directorio page_dir
-uint32_t* get_page_table_entry(uint32_t page_dir[], void* virtual) {
-    uint32_t dir_entry = page_dir[PDI(virtual)];
+// Devuelve un puntero a la entrada en la tabla de paginas correspondiente a
+// la direccion virtual ``virt`` usando el directorio ``pd``.
+uint32_t* get_pte(uint32_t pd[], void* vaddr) {
+    uint32_t pde = pd[PDI(vaddr)];
 
     //Si la tabla de paginas no esta disponible, retornar NULL
-    if (!(dir_entry | PDE_P)) return NULL;
+    if (!(pde & PDE_P))
+        return NULL;
 
-    uint32_t *table = KVIRTADDR( PDE_PT_BASE(dir_entry) );
-    return &table[PTI(virtual)];
+    uint32_t *pt = KVIRTADDR(PDE_PT_BASE(pde));
+    return &pt[PTI(vaddr)];
 }
 
 // Mapea una pagina fisica nueva para una tabla de paginas de page_dir
-void allocate_page_table(uint32_t page_dir[], void* virtual) {
-    page *res = reserve_page(page_list->next);
-    void *phisical = PAGE_TO_PHADDR(res);
-    page_dir[PDI(virtual)] = PDE_PT_BASE(phisical) | PDE_P | PDE_PWT;
-    memset(KVIRTADDR(phisical), 0, PAGE_SIZE);
+void allocate_pt(uint32_t pd[], void* vaddr) {
+    page *page = reserve_page(page_list->next);
+    void *phpage = PAGE_TO_PHADDR(page);
+    pd[PDI(vaddr)] = PDE_PT_BASE(phpage) | PDE_P | PDE_PWT;
+    memset(KVIRTADDR(phpage), 0, PAGE_SIZE);
 }
 
-// Mapea una pagina fisca nueva para la direccion virtual, pasada por parametro 
-void* new_page(uint32_t page_dir[], void* virtual, uint32_t flags) {
+// Mapea una pagina fisica nueva para la direccion virtual pasada por parametro 
+void* new_page(uint32_t pd[], void* vaddr, uint32_t flags) {
     /*
     TODO: Si no hay mas paginas libres kpanic!!!
     if (!page_list) kpanic("No hay mas memoria fisica disponible");
     */  
 
     // Si la tabla de paginas no estaba presente mapearla
-    if (!(page_dir[PDI(virtual)] | PDE_P))
-        allocate_page_table(page_dir, virtual);
+    if (!(pd[PDI(vaddr)] & PDE_P))
+        allocate_pt(pd, vaddr);
 
-    uint32_t *entry = get_page_table_entry(page_dir, virtual);
-    page *res = reserve_page(page_list->next);
-    void *phisical = PAGE_TO_PHADDR(res);
+    page *page = reserve_page(page_list->next);
+    void *phpage = PAGE_TO_PHADDR(page);
 
-    *entry = PTE_PAGE_BASE(phisical) | PTE_P | flags;
+    uint32_t *pte = get_pte(pd, vaddr);
+    *pte = PTE_PAGE_BASE(phpage) | PTE_P | flags;
 
-    return virtual; 
+    return vaddr; 
 }
 
-// Retorna la pagina fisica correspondiente a la direccion virtual, a la lista de paginas libres
-void free_page(uint32_t page_dir[], void* virtual) {
-    uint32_t *entry = get_page_table_entry(page_dir, virtual);
-    page* fpage = PHADDR_TO_PAGE( PTE_PAGE_BASE(*entry) );
+// Retorna la pagina fisica correspondiente a la direccion virtual a la lista
+// de paginas libres
+void free_page(uint32_t pd[], void* vaddr) {
+    uint32_t *pte = get_pte(pd, vaddr);
+    page* page = PHADDR_TO_PAGE(PTE_PAGE_BASE(*pte));
 
-    return_page(fpage);
-    *entry = 0;
+    return_page(page);
+    *pte = 0;
 }
 
 
-// Decrementa el contador de referencias y retorna la pagina a la lista de paginas libres al llegar a 0
-void return_page(page* returned) {
-    if ( --returned->count == 0 ) {
+// Decrementa el contador de referencias y retorna la pagina a la lista de
+// paginas libres si este llego a cero
+void return_page(page* reserved) {
+    reserved->count--;
+    if (reserved->count == 0) {
         if (page_list)
-            add_page_to_list(page_list, returned);
+            add_page_to_list(page_list, reserved);
         else
-            page_list = returned->next = returned->prev = returned;
+            page_list = reserved->next = reserved->prev = reserved;
     }
 }
 
-// Saca a la pagina de la lista de paginas libres e incrementa el contador de referencias
-page *reserve_page(page* reserved) {
-    // Si tiene nodos adyacentes linkearlos entre si
-    if (reserved->next && reserved->prev)
-        link_pages(reserved->prev, reserved->next);
+// Saca a la pagina de la lista de paginas libres e incrementa su contador de
+// referencias
+page *reserve_page(page* page) {
+    // Si tiene nodos adyacentes enlazarlos entre si
+    if (page->next && page->prev)
+        link_pages(page->prev, page->next);
 
-    // Si la pagina era la primera de la lista, poner a otra como primera
-    if (reserved == page_list) {
-        if ( reserved->next != reserved )
-            page_list = reserved->next;
+    // Si la pagina era la cabeza de la lista, cambiar la cabeza por otra
+    if (page_list == page) {
+        if (page->next != page)
+            page_list = page->next;
         else page_list = NULL;
     }
 
-    // Incrementar contador, y marcar nodos adyacentes como nulos
-    reserved->count++;
-    reserved->next = reserved->prev = NULL;
+    // Incrementar contador, y marcar adyacencias como nulas
+    page->count++;
+    page->next = page->prev = NULL;
 
-    return reserved;
+    return page;
 }
 
