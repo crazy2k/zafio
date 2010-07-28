3bd2b82ac0c81705ae5f35603aa2564087a52049
Pablo Antonio <pabloa@gmail.com>
2010-07-28 05:57:20 -0300

    Hacer cambios esteticos en vmm.c

--- src/kernel/vmm.c
+++ src/kernel/vmm.c
@@ -13,23 +13,24 @@ memory_info_t memory_info;
 
 
 void vm_init() {
-    //Mapeamos los primeros 4MB fisicos, desde el comienzo de la memoria fisica
-    map_kernel_pages(kernel_pd, (void *) 0x00000000 + KERNEL_OFFSET, 1024);
+    // Mapeamos los primeros 4MB fisicos, desde el comienzo de la memoria fisica
+    map_kernel_pages(kernel_pd, (void *)0x00000000 + KERNEL_OFFSET, 1024);
 
-    //Limite de direcciones virtuales mapeables
-    void* vaddr_limit = KVIRTADDR(PAGE_TO_PHADDR(memory_info.last_page + 1));
-    if (vaddr_limit > LAST_KERNEL_VADDR) vaddr_limit = LAST_KERNEL_VADDR;
+    // Limite de direcciones virtuales mapeables en el higher half
+    void *vaddr_limit = KVIRTADDR(PAGE_TO_PHADDR(memory_info.last_page + 1));
+    if (vaddr_limit > LAST_KERNEL_VADDR)
+        vaddr_limit = LAST_KERNEL_VADDR;
 
-    //Cantidad de memoria q va a ser mapeada
-    int total_memory = ((uint32_t) vaddr_limit - KERNEL_OFFSET) - PAGE_4MB_SIZE;
-    //Desde donde se va a mapear memoria
-    void *mem_vaddr = (void*) PAGE_4MB_SIZE + KERNEL_OFFSET;
+    // Cantidad de memoria que va a ser mapeada (en bytes)
+    int total_memory = ((uint32_t)vaddr_limit - KERNEL_OFFSET) - PAGE_4MB_SIZE;
+    // Desde donde se va a mapear memoria
+    void *mem_vaddr = (void*)PAGE_4MB_SIZE + KERNEL_OFFSET;
 
     map_kernel_tables(kernel_pd, mem_vaddr, page_tables, 
-        ((uint32_t) ALIGN_TO_4MB(total_memory, TRUE) )/PAGE_4MB_SIZE);
+        ((uint32_t)ALIGN_TO_4MB(total_memory, TRUE))/PAGE_4MB_SIZE);
 
     map_kernel_pages(kernel_pd, mem_vaddr, 
-        ((uint32_t) ALIGN_TO_PAGE(total_memory, TRUE) )/PAGE_SIZE);
+        ((uint32_t)ALIGN_TO_PAGE(total_memory, TRUE))/PAGE_SIZE);
 
     // Quitamos el mapeo de los primeros 4MB del espacio de direcciones virtual
     page_dir_unmap(kernel_pd, (void *)0x00000000);
