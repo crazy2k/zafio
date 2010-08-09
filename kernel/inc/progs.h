db6a122f25d86f1b99b034145d77db278977bb71
Pablo Herrero <pablodherrero@gmail.com>
2010-08-09 01:45:38 -0300

    Agregar funcion load_task_stack

--- kernel/inc/progs.h
+++ kernel/inc/progs.h
@@ -3,7 +3,6 @@
 #define __PROGS_H__
 
 #include "sched.h"
-#include "elf_helpers.h"
 
 struct program_t {
     char *name;
@@ -20,7 +19,10 @@ struct task_t;
 
 void load_task_image(struct task_t* );
 
+void load_task_stack(struct task_t* task);
+
 #define CODE_PAGE_FLAGS (PTE_P | PTE_PWT | PTE_US)
 #define DATA_PAGE_FLAGS (PTE_P | PTE_PWT | PTE_US | PTE_RW) 
+#define STACK_PAGE_FLAGS DATA_PAGE_FLAGS
 
 #endif
--- kernel/src/progs.c
+++ kernel/src/progs.c
@@ -1,6 +1,15 @@
 #include "../inc/progs.h"
+#include "../inc/elf_helpers.h"
+#include "../inc/mmu.h"
+#include "../inc/vmm.h"
 
 
 void load_task_image(task_t * task) {
     load_elf_proc_image(task->prog->file, task->pd); 
+}
+
+void load_task_stack(task_t* task) {
+    void *stack_page = ALIGN_TO_PAGE(elf_stack_bottom(task->prog->file), FALSE) -
+        PAGE_SIZE;
+    new_page(task->pd, stack_page, STACK_PAGE_FLAGS);
 }
