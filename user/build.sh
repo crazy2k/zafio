7b95cf395ca63bb93e1427e41dff59d336671935
Pablo Antonio <pabloa@gmail.com>
2010-08-10 02:52:04 -0300

    Agregar script pobre para compilacion de codigo de usuario

--- user/build.sh
+++ user/build.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
 
-gcc -nostdlib -nostdinc -nostartfiles -nodefaultlibs -c sec.c 
-ld -e main sec.o -o sec
+for x in *.c
+do
+    exe=${x%%.c}
+    rm -f "$exe" "$exe.o"
+
+    gcc -nostdlib -nostdinc -nostartfiles -nodefaultlibs -c "$exe.c"
+    ld -e main "$exe.o" -o "$exe"
+done
