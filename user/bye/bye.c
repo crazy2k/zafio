b44396331493c44c96ba16aa1a48cabd5a1164a9
Pablo Antonio <pabloa@gmail.com>
2010-11-29 05:53:30 -0300

    Eliminar magic breakpoint de la tarea bye

--- user/bye/bye.c
+++ user/bye/bye.c
@@ -2,8 +2,6 @@ char *val = "Chau!\n";
 
 int main() {
     while(1) {
-        __asm__ __volatile__("xchg %bx, %bx");
-
         // Llamada al sistema 4
         __asm__ __volatile__("mov %0, %%eax" : : "i" (4));
         // Primer parametro
