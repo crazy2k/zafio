608aa7c1c3a519f73b4f2cf19639f1edf06d0625
Pablo Herrero <pablodherrero@gmail.com>
2011-01-26 03:15:21 -0300

    Updated user shell

--- user/inc/io.h
+++ user/inc/io.h
@@ -1,2 +1,14 @@
-int read_line(int dev, char* buff, int buff_size);
-void write_line(int dev, char* str);
+#ifndef __IO_H__
+
+#define __IO_H__
+
+#define TERM_INPUT 0
+#define TERM_OUTPUT 1
+
+int read_line(char* str, int buff_size);
+void write_line(char* str);
+
+int read(int dev, char* buff, int buff_size);
+void write(int dev, char* str);
+
+#endif
--- user/inc/utils.h
+++ user/inc/utils.h
@@ -1,1 +1,7 @@
+#ifndef __UTILS_H__
+
+#define __UTILS_H__
+
 int strcmp(char* left, char* right);
+
+#endif
