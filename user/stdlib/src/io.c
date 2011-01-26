792524eff1f61149d89f0cec05e541450d884e83
Pablo Herrero <pablodherrero@gmail.com>
2011-01-26 07:59:09 -0300

    Agregar read_line

--- user/stdlib/src/io.c
+++ user/stdlib/src/io.c
@@ -6,8 +6,43 @@
 #define READ_BUFF_LEN 1024
 
 int read_line(char* str, int buff_size) {
-    static char* buff[READ_BUFF_LEN] = {0};
+    static char read_buff[READ_BUFF_LEN] = {0};
+    static int buff_beg = 0, 
+               buff_end = 0;
+           
+    int newline = -1, count = 0;
 
+    if ((READ_BUFF_LEN - buff_end) >= buff_size)
+        count = read(TERM_INPUT, &read_buff[buff_end], buff_size);
+    else {
+        count = read(TERM_INPUT, &read_buff[buff_end], READ_BUFF_LEN - buff_end);
+        count += read(TERM_INPUT, read_buff, buff_size - count);
+    }
+    
+    if (count == -1)
+        return -1;
+    
+    buff_end = (buff_end + count) % READ_BUFF_LEN;
+
+    for (int i = 0; i < count; i++) {
+        if (read_buff[buff_beg + i]) {
+            newline = buff_beg + i;
+            break;
+        }
+    }
+    
+    count = (newline - buff_beg) % READ_BUFF_LEN;
+    
+    if (newline >= buff_beg)
+        memcpy(str, &read_buff[buff_beg], newline - buff_beg + 1);
+    else {
+        memcpy(str, &read_buff[buff_beg], READ_BUFF_LEN - buff_beg);
+        memcpy(str, read_line, newline + 1);
+    }
+    
+    buff_beg = (newline + 1) % READ_BUFF_LEN;
+    
+    return count;
 }
 
 void write_line(char* str) {
