1feecbc7fcaa79317cf495d3c1b6af725914dd3a
Pablo Antonio <pabloa@gmail.com>
2011-01-27 04:10:49 -0300

    Actualizar testkb

--- user/testkb/testkb.c
+++ user/testkb/testkb.c
@@ -1,15 +1,18 @@
 #include <types.h>
 #include <syscalls.h>
+#include <io.h>
 
 char buf[1024] = { 0 };
+char *msg = "Holasss!\n";
 
 int main() {
     while (1) {
-        int n = syscall(3, 0, (uint32_t)buf, 1024, 0, 0);
+        //write(1, msg, 1024);
+        int n = read(0, buf, 1024);
 
         buf[n] = '\0';
         if (n > 0) {
-            syscall(4, (uint32_t)buf, 0, 0, 0, 0);
+            write(1, buf, 1024);
         }
     }
 
