0504f2826727a15bdbcd9621ae3feb032272917b
Pablo Antonio <pabloa@gmail.com>
2011-02-17 08:01:55 -0300

    Agregar programa estupido que saluda

--- user/hello/hello.c
+++ user/hello/hello.c
@@ -1,7 +1,23 @@
+#include <io.h>
+
+char line_buffer[1024];
+
 int main() {
-    int i;
-    for (i = 0; i < 9999999; i++)
-        ;
+    devreq(KEYBOARD);
+    devreq(SCREEN);
+    devreq(TERMINAL);
+
+    write(TERMINAL, "Nombre: ", 9);
+
+    read_line(line_buffer, 1024);
+
+    write(TERMINAL, "Hola ", 6);
+    write(TERMINAL, line_buffer, 1024);
+
+    devrel(KEYBOARD);
+    devrel(SCREEN);
+    devrel(TERMINAL);
+
     return 0;
 }
 
